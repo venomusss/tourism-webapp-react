9a15125adbe03471dc06b77270276ff794b8bd36
venomusss <vovamushii@gmail.com>
2022-04-15 14:59:33 +0300

    [T23] fix post item links

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -36,13 +36,13 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
     
     const ratingToRender = post.rating.find(rate => rate.userId === currentUser?.uid)?.value || 0
     return (
-        <div className="gray-container post-item" onClick={handlePostDetailNavigate}>
-            <div className="post-item-img-container">
+        <div className="gray-container post-item">
+            <div className="post-item-img-container" onClick={handlePostDetailNavigate}>
                 <img alt='' className="post-item_img" src={post.images[0]}/>
             </div>
             <div className="white-container last">
                 <div className="item-text">
-                    <div className="post-item_title">{post.name}</div>
+                    <div onClick={handlePostDetailNavigate} className="post-item_title">{post.name}</div>
                     <div className="post-item_rate-section">
                         <Rating initialRating={ratingToRender} onClick={(value) => post.rating.find(rate => rate.userId === userId) ? handleRateChange(value) : handleRateClick(value)} />
                         <label>{Math.floor(post.cachedRating)}</label>
