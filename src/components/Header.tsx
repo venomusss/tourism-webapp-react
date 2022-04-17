0aa04a5b918ff4a0ed5dda57cc4f2fa18a392005
venomusss <vovamushii@gmail.com>
2022-04-17 15:57:41 +0300

    [T22] Fix header burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,17 +12,17 @@ const Header: React.FC = () => {
             setDbUser(user)
         })
     }, [user?.uid])
+    const openPurger = () => {
+        const menu : null| HTMLElement = document.getElementById('menu');
+        menu!.classList.toggle('active');
+    }
     return (
         <header className="header">
             <NavLink to={"/"}>
                 <div className='logo'><img className='logo-img' alt='' src='../imgs/green.png'/></div>
             </NavLink>
-            <input id="menu-toggle" type="checkbox"/>
-            <label className='menu-button-container' htmlFor="menu-toggle">
-                <div className='menu-button'/>
-            </label>
             {user ?
-                <ul className="menu">
+                <ul  onClick={openPurger} id='menu' className="menu">
                     {dbUser?.role === 'ADMIN' ?
                         <>
                             <NavLink to='/requests'>Admin Panel</NavLink>
@@ -34,14 +34,20 @@ const Header: React.FC = () => {
                     <NavLink to='/login' onClick={() => logOut()}>Log Out</NavLink>
                     <NavLink className='profile' to='/profile'>
                         <div className="avatar">{dbUser?.name.substr(0, 1)}</div>
+                        <div className="profile-link-text">Profile</div>
                     </NavLink>
                 </ul>
                 :
-                <ul className="menu">
+                <ul  onClick={openPurger}  id='menu' className="menu">
                     <NavLink to='/posts'>Home</NavLink>
                     <NavLink to='/login'>Log In</NavLink>
                     <NavLink to='/signup'>Sign Up</NavLink>
                 </ul>}
+                <svg onClick={openPurger} fill='white' className='header-burger' viewBox="0 0 100 80" width="70" height="70">
+                    <rect width="100" height="15"/>
+                    <rect y="30" width="100" height="15"/>
+                    <rect y="60" width="100" height="15"/>
+                </svg>
         </header>
     );
 };
